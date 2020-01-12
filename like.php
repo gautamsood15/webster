ccb9d5f0e36213ad4d847bbb67dc94efe616285f
gautamsood15 <gauty22@gmail.com>
2020-01-12 19:31:23 +0530

    Like Button Part 1

---
+++ like.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<?php
+
+	require 'config/config.php';
+	include("includes/classes/User.php");
+	include("includes/classes/Post.php");
+
+	//check if in session and then get user data for the defiened user name
+
+	if(isset($_SESSION['username'])) {                    
+		$userLoggedIn = $_SESSION['username'];
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+		$user = mysqli_fetch_array($user_details_query);
+	 
+		}
+
+	else {
+		header("Location: register.php");
+		}
+
+
+	if (isset($_GET['post_id'])) {
+		$post_id = $_GET['post_id'];
+	}
+
+
+	$get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE id='post_id'");
+	$row = mysqli_fetch_array($get_likes);
+	$total_likes = $row['likes'];
+	$user_liked = $row['added_by'];
+
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'");
+	$row = mysqli_fetch_array($user_details_query);
+
+
+	$check_query = mysqli_query($con, "SELECT * FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
+	$num_rows = mysqli_num_rows($check_query);
+
+	if ($num_rows > 0) {
+		echo " ";
+	} else {
+		echo " ";
+	}
+
+	?>
+
+
+</body>
+</html>
+
+
+
+
