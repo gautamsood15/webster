7982ecccecf5e90ccdf6f18a6b30dbec814ecc17
gautamsood15 <gauty22@gmail.com>
2020-01-17 03:23:02 +0530

    Like Button Styling

--- like.php
+++ like.php
@@ -6,6 +6,25 @@
 </head>
 <body>
 
+
+	<style type="text/css">
+		
+		* {
+			font-family: Arial, Helvetica, Sans-serif;
+		}	
+
+		body {
+			background-color: #fff;
+		}
+
+		form {
+			position: absolute;
+			top: 0;
+		
+			}
+
+	</style>
+
 	<?php
 
 	require 'config/config.php';
