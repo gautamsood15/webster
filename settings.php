c9aed6d659fe5b548a04385afecb1b89c712a669
gautamsood15 <gauty22@gmail.com>
2020-03-17 13:08:02 +0530

    linking to upload profile picture page

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+include("includes/header.php");
+//include("includes/settings_handler.php");
+?>
+
+<div class="main_column column">
+	
+	<h4>Account Settings</h4>
+	<?php
+	echo "<img src='" . $user['profile_pic'] ."' id='small_profile_pics'>";
+	?>
+	<br>
+	<a href="upload.php">Upload new Profile picture</a> <br><br><br>
+
+	
+</div>
