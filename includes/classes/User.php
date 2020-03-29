73ef1c6540329ffca277e9bc03d06bb21d420eb7
gautamsood15 <gauty22@gmail.com>
2020-03-29 09:00:56 +0530

    Infinite sccrolling system - Final

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -1,14 +1,13 @@
 <?php 
 
-class User {
+	class User {
 	private $user;
 	private $con;
 
-	public function __construct($con, $user) {
+	public function __construct($con, $user){
 		$this->con = $con;
 		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user'");
 		$this->user = mysqli_fetch_array($user_details_query);
-
 	}
 
 	public function getUsername() {
@@ -20,11 +19,10 @@ class User {
 		$query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
 		$row = mysqli_fetch_array($query);
 		return $row['num_posts'];
-
 	}
 
 	public function getFirstAndLastName() {
-		$username =  $this->user['username'];
+		$username = $this->user['username'];
 		$query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE username='$username'");
 		$row = mysqli_fetch_array($query);
 		return $row['first_name'] . " " . $row['last_name'];
@@ -35,12 +33,14 @@ class User {
 		$query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE username='$username'");
 		$row = mysqli_fetch_array($query);
 
-		if ($row['user_closed'] == 'yes')
+		if($row['user_closed'] == 'yes')
 			return true;
-		else
+		else 
 			return false;
 	}
+
 }
+
 
 
 ?>
--- index.php
+++ index.php
@@ -1,111 +1,106 @@
-<?php
-	include("includes/header.php");
-	include("includes/classes/User.php");
-	include("includes/classes/Post.php");
+<?php 
+include("includes/header.php");
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
 
 
+if(isset($_POST['post'])){
+	$post = new Post($con, $userLoggedIn);
+	$post->submitPost($_POST['post_text'], 'none');
+}
 
-	if (isset($_POST['post'])) {
-		$post = new Post($con, $userLoggedIn);
-		$post->submitPost($_POST['post_text'], 'none');
 
-		header("Location: index.php");
-	}
-?>
+ ?>
+	<div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
-		<div class="user_details column">
-			
-			<a href="<?php echo $userLoggedIn ?>"> <img src="<?php echo $user['profile_pic']; ?>"> </a>
+		<div class="user_details_left_right">
+			<a href="<?php echo $userLoggedIn; ?>">
+			<?php 
+			echo $user['first_name'] . " " . $user['last_name'];
 
-			<div class="user_details_left_right">
-				<a href="<?php echo $userLoggedIn ?>">
-					<?php echo $user['first_name'] . " " . $user['last_name']; ?>
-				</a>
-				<br>
-				<?php echo "Posts: " . $user['num_posts']. "<br>";
-				echo "Likes: " . $user['num_likes']; 
-				?>
+			 ?>
+			</a>
+			<br>
+			<?php echo "Posts: " . $user['num_posts']. "<br>"; 
+			echo "Likes: " . $user['num_likes'];
 
-			</div>
+			?>
 		</div>
 
-		<div class="main_column column">
-			<form class="post_form" action="index.php" method="POST">
-				
-				<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
-				<input type="submit" name="post" id="post_button" value="Post">
-				<hr>
+	</div>
 
-			</form>
-			
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
 
-			<div class="posts_area"></div>
-			<img id="loading" src="assets/images/icons/loading.gif">
+		</form>
 
-		</div>
-
-		<script>
-
-			var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+		<div class="posts_area"></div>
+		<img id="loading" src="assets/images/icons/loading.gif">
 
 
-			$(document).ready(function() {
+	</div>
 
+	<script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('#loading').show();
+
+		//Original ajax request for loading first posts 
+		$.ajax({
+			url: "includes/handlers/ajax_load_posts.php",
+			type: "POST",
+			data: "page=1&userLoggedIn=" + userLoggedIn,
+			cache:false,
+
+			success: function(data) {
+				$('#loading').hide();
+				$('.posts_area').html(data);
+			}
+		});
+
+		$(window).scroll(function() {
+			var height = $('.posts_area').height(); //Div containing posts
+			var scroll_top = $(this).scrollTop();
+			var page = $('.posts_area').find('.nextPage').val();
+			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+			if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
 				$('#loading').show();
-			
-				// Original ajax request for loading first posts
-				$.ajax({
+
+				var ajaxReq = $.ajax({
 					url: "includes/handlers/ajax_load_posts.php",
 					type: "POST",
-					data: "page=1$userLoggedIn=" + userLoggedIn,
-					cache: false,
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
 
-					success: function(data) {
+					success: function(response) {
+						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+
 						$('#loading').hide();
-						$('.posts_area').html(data);
+						$('.posts_area').append(response);
 					}
-
 				});
 
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
 
 
-				$(window).scroll(function() {
-					var height = $('.posts_area').height(); // DIV containing posts
-					var scroll_top = $(this).scrollTop();
-					var page = $('.posts_area').find('.nextPage').val();
-					var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+	});
+
+	</script>
 
 
-					if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
 
-						$('loading').show();
-
-						var ajaxReq = $.ajax({
-							url: "includes/handlers/ajax_load_posts.php",
-							type: "POST",
-							data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-							cache: false,
-
-							success: function(response) {
-								$('.posts_area').find('.nextPage').remove(); //Removes current .nextPage
-								$('.posts_area').find('.noMorePosts').remove(); 
-
-								$('#loading').hide();
-								$('.posts_area').append(response);
-							}
-
-						});	
-					}  // End if
-
-					return false;
-
-				});  // End (window).scroll(function()
-
-
-			});
-
-
-		</script>
 
 	</div>
 </body>
