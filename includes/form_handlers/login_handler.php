02236de456246b8c0379141007226afc612a8ad4
gautamsood15 <gauty22@gmail.com>
2020-05-02 11:17:05 +0530

    Reopening a closed account

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -14,6 +14,16 @@
 			$row = mysqli_fetch_array($checl_database_query);
 			$username = $row['username'];
 
+
+// Reopen a closed account by simply logging in 
+
+			$user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+
+			if (mysqli_num_rows($user_closed_query) == 1) {
+
+				$reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
+			}
+
 			$_SESSION['username'] = $username;
 			header("Location: index.php");                     // redirects the page to index.php once login
 			exit();
