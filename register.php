b17040fc2c3798c70048e038ce41fbf5b6e5a5f7
gautamsood15 <gauty22@gmail.com>
2020-05-16 20:36:05 +0530

    Validating name and password

--- register.php
+++ register.php
@@ -66,7 +66,43 @@
 		else {
 			echo "Emails don't match";
 		}
+	
+
+//---------------------------------------------------------------------------------
+
+
+		if(strlen($fname) > 25 || strlen($fname) < 2) {
+			echo "Your First Name must be between 2 and 25 characters";
+		}
+
+		if(strlen($lname) > 25 || strlen($lname) < 2) {
+			echo "Your Last Name must be between 2 and 25 characters";
+		}		
+
+
+//----------------------------------------------------------------------------------
+
+		if($password != $password2) {
+			echo "Your passwords do not match";
+		}
+
+		else {
+			if(preg_match('/[^A-Za-z0-9]/', $password)) {
+				echo "Your password can only contain english characters or numbers";
+			}
+		}
+
+		if (strlen($password > 30) || strlen($password) < 5) {
+			echo "Your password must be between 5 and 30 characters";
+		}
+
+		
+
+
 	}
+
+
+
 
 ?>
 
