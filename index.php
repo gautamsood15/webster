9d478a188c2052ef79b50a371fa259af7a52b4ea
gautamsood15 <gauty22@gmail.com>
2020-05-26 16:38:09 +0530

    Adding DB connection code

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+	$con = mysqli_connect("localhost", "root", "", "TEST_CASE");
+
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
