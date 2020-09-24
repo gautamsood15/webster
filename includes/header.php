f1ff3afdd315c79bd159716b140100da8bb77f30
gautamsood15 <gauty22@gmail.com>
2020-09-24 13:38:07 +0530

    Adding twitter bootstrap

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,24 @@
-<?php
-  
+ <?php
+
   require 'config/config.php';
  
   if(isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
   }
-  else {
+ /* else {
     header("Location: register.php");
   }
+*/
 
 ?> 
+
 
 <html>
 <head>
 	<title>Welcome to Webster!</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 </head>
 <body>
+
