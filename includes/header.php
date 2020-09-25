0776996b00812ead52420fff05ffc0ec6f91b282
gautamsood15 <gauty22@gmail.com>
2020-09-25 17:51:46 +0530

    Navigation bar icons

--- includes/header.php
+++ includes/header.php
@@ -2,11 +2,15 @@
 
   require 'config/config.php';
 
- 
-  if(isset($_SESSION['username'])) {
+/*
+  if(isset($_SESSION['username'])) {                    */      
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
+/* 
   }
- /* else {
+
+  else {
     header("Location: register.php");
   }
 */
@@ -24,6 +28,7 @@
 	<script src="assets/js/bootstrap.js"></script>
 	
 	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
@@ -34,6 +39,29 @@
 		<div class="logo">
 			<a href="index.php">Webster!</a>
 		</div>
+
+		<nav>
+			<a href="#">
+				<?php echo $user['first_name']; ?>
+			</a>
+			<a href="#">
+				<i class="fa fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-envelope fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-bell fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-user fa-lg"></i>
+			</a>	
+			<a href="#">
+				<i class="fa fa-cog fa-lg"></i>
+			</a>			
+		</nav>
+
+
 	</div>
 
 
