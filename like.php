fee43993801a6092f43224db8e09e89fe45dee76
gautamsood15 <gauty22@gmail.com>
2020-10-18 20:10:02 +0530

    Like Button Part 2

--- like.php
+++ like.php
@@ -44,9 +44,22 @@
 	$num_rows = mysqli_num_rows($check_query);
 
 	if ($num_rows > 0) {
-		echo " ";
+		echo "<form action="like.php?post_id=' . $post_id . '" method="POST"> 
+				<input type="submit" class="comment_like" name="unlike_button" value="Unlike">
+				<div class="like_value">
+					'. $total_likes .' Likes
+				</div>
+			</form>
+		";
 	} else {
-		echo " ";
+		echo "<form action="like.php?post_id=' . $post_id . '" method="POST"> 
+				<input type="submit" class="comment_like" name="like_button" value="Like">
+				<div class="like_value">
+					'. $total_likes .' Likes
+					
+				</div>
+			</form>
+		";
 	}
 
 	?>
