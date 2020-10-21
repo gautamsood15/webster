927dd1d44f2c68ea6fe08e9d122626d1caa8a970
gautamsood15 <gauty22@gmail.com>
2020-10-22 02:54:08 +0530

    Remove Friend Functionality

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -84,6 +84,20 @@
 
 	}
 
+	public fucntion removeFriend($user_to_remove) {
+		$logged_in_user = $this->user['username'];
+
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$user_to_remove'");
+		$row = mysqli_fetch_query($query);
+		$friend_arry_username = $row['friend_array'];
+
+		$new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_array']);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$logged_in_user'");
+
+		$new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+	}
+
 }
 
 
