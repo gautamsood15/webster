bce2a2a242866d864b0f0fe08fee089e657036c5
gautamsood15 <gauty22@gmail.com>
2020-11-03 20:47:13 +0530

    Infinite Scrolling - Part 1

--- includes/header.php
+++ includes/header.php
@@ -75,4 +75,58 @@ else {
 	</div>
 
 
+	<script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$(window).scroll(function() {
+			var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+			var scroll_top = $('.dropdown_data_window').scrollTop();
+			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+
+			if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+
+				var pageName; // Holds name of page to send ajax request to
+				var type = $('#dropdown_data_type').val();
+
+				if (type == 'motification')
+					pageName = "ajax_load_notifications.php";
+				else if(type = 'message')
+					pageName = "ajax_load_message.php"
+
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/ajax_load_posts.php",
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+
+						$('#loading').hide();
+						$('.posts_area').append(response);
+					}
+				});
+
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
+
+
+	});
+
+	</script>
+
+
+
+
+
+
+
 	<div class="wrapper">
