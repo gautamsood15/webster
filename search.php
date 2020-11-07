a91c6f7e34305be74fa99cf3fdb40444f90c512d
gautamsood15 <gauty22@gmail.com>
2020-11-08 03:41:48 +0530

    Displaying the Friend button next to each result

--- search.php
+++ search.php
@@ -63,6 +63,22 @@
 
 			if ($user['username'] != $row['username']) {
 				
+				//Generate button depending on friendship status
+				if ($user_obj->isFriend($row['username']))
+					$button = "<input type='submit' name='" . $row['username'] . "' class='danger' value='Remove Friend'>";
+				else if ($user_obj->didReceiveRequest($row['username']))
+					$button = "<input type='submit' name='" . $row['username'] . "' class='warning' value='Respond to request'>";
+				else if ($user_obj->didSendRequest($row['username']))
+					$button = "<input class='default' value='Request Sent'>";
+				else
+					$button = "<input type='submit' name='" . $row['username'] . "' class='success' value='Add Friend'>";
+
+				$mutual_friends = $user_obj->getMutualFriends($row['username']) . " friends in common"
+
+
+				//Button forms
+
+
 			}
 
 		}
