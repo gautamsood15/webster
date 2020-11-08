d28cda554ff259ec8bf1fc5ddd9a6ee617eb1206
gautamsood15 <gauty22@gmail.com>
2020-11-08 21:30:49 +0530

    Embedded youtube link bug fix

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -17,16 +17,20 @@ class Post {
 
 			$body_array = preg_split("/\s+/", $body);
 
-			foreach ($body_array as $key => $value) {
-				
-				if (strpos($value, "www.youtube.com/watch?v=") != false) {
-					
-					$value = preg_replace("!watch\?v=!", "embed/", $value);
+			foreach($body_array as $key => $value) {
+
+				if(strpos($value, "www.youtube.com/watch?v=") !== false) {
+
+					$link = preg_split("!&!", $value);
+					$value = preg_replace("!watch\?v=!", "embed/", $link[0]);
 					$value = "<br><iframe width=\'420\' height=\'315\' src=\'" . $value ."\'></iframe><br>";
 					$body_array[$key] = $value;
+
 				}
+
 			}
 			$body = implode(" ", $body_array);
+
 
 
 
